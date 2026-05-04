f8d9a20f3ac1bac15da52eb98cfa93398424b02c
casablanque-code <casablanque@proton.me>
2026-05-04 22:18:28 +0300

    fix: include .wasm files, remove nested wasm/wasm dir

---
+++ web/wasm/netconv_wasm.d.ts
@@ -0,0 +1,48 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* Основная функция — вызывается из JS
+* convert_config(source_config: string, source_vendor: string, target_vendor: string) → JSON string
+* @param {string} source_config
+* @param {string} source_vendor
+* @param {string} target_vendor
+* @returns {string}
+*/
+export function convert_config(source_config: string, source_vendor: string, target_vendor: string): string;
+/**
+*/
+export function init(): void;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly convert_config: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => void;
+  readonly init: () => void;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
